efcac7f67080aad5d1539c95ce108359e54815ee
Taylor Covington <tcovingtondev@gmail.com>
2019-12-27 07:51:25 -0700

    added User model and express user.js validation for name, email, password

---
+++ routes/api/users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
+
+// @route  GET api/users
+// @desc   Test route
+// @access Public (don't need a token to access)
+// router.get('/', (req, res) => res.send('User route'));
+
+// @route  POST api/users
+// @desc   Register User
+// @access Public (don't need a token to access)
+router.post('/', [
+  //validations using express-validator
+  check('name', 'Name is required')
+  .not()
+  .isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Pleae enter a password with 6 or more characters'
+  ).isLength({ min: 6 }) 
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send('User route'); 
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,18 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
                             // sends data to the browser
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // look for port to get the port number, if none it dfaults to 5000
 const PORT = process.env.PORT || 5000;
