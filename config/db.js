c043dd74ffb11aa5b7152c985adf4e54eb3ba701
Taylor Covington <tcovingtondev@gmail.com>
2019-12-27 07:04:58 -0700

    connected MongoDB to server

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+//anytime we try an async await we want to put inside a try, catch just in case we get an error
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+
+    console.log('MongoDB Connected...')
+  } catch(err) {
+    console.log(err.message);
+    // Exit process w/ failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // bring in express
 const express = require('express');
+const connectDB = require('./config/db');
 
 
 const app = express();
+
+// Connect Database
+connectDB();
 
                             // sends data to the browser
 app.get('/', (req, res) => res.send('API Running'));
