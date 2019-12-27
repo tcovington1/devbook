aedcf857708ac9dd949b11b6ec85aaa0cabb6b3f
Taylor Covington <tcovingtondev@gmail.com>
2019-12-27 08:18:50 -0700

    users.js registration added and working in postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check')
+
+const User = require('../../models/User');
 
 // @route  GET api/users
 // @desc   Test route
@@ -18,12 +22,54 @@ router.post('/', [
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Pleae enter a password with 6 or more characters'
   ).isLength({ min: 6 }) 
-], (req, res) => {
+],
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  res.send('User route'); 
+
+  const { name, email, password } = req.body;
+
+    // mongoose - promise using async await
+    try {
+      //* See if user exists
+      let user = await User.findOne({ email });
+
+      //* If user exists we will send error
+      if(user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+      }
+
+      //* Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      //* Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+        // this finally saves the user
+      await user.save();
+
+      //* Return jsonwebtoken
+
+    res.send('User registered'); 
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
